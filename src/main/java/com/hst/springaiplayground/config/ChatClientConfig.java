ec50e41c6be50f35c7c33c0cb6c6462ef9c0c890
hyungyu-lee <gusrb0808@naver.com>
2025-10-31 21:12:54 +0900

    Streaming 기능 테스트

--- src/main/java/com/hst/springaiplayground/config/ChatClientConfig.java
+++ src/main/java/com/hst/springaiplayground/config/ChatClientConfig.java
@@ -10,7 +10,7 @@ public class ChatClientConfig {
     @Bean
     public ChatClient openAIClient(ChatClient.Builder chatClientBuilder) {
         return chatClientBuilder
-                .defaultSystem("당신은 유치원 교사입니다. 아이들을 대하듯한 어조로 이야기하세요.")
+                .defaultSystem("당신은 최고 수준의 머니 컨설턴트입니다. 반드시 대답할 때 한국어로 대답하고, 존댓말 50자로 대답하세요.")
                 .build();
     }
 
--- src/main/java/com/hst/springaiplayground/web/ChatController.java
+++ src/main/java/com/hst/springaiplayground/web/ChatController.java
@@ -1,11 +1,11 @@
 package com.hst.springaiplayground.web;
 
 import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 
 @RestController
 @RequestMapping("/chat")
@@ -16,12 +16,12 @@ public class ChatController {
         this.openAIChatClient = openAIChatClient;
     }
 
-    @GetMapping
-    public ChatResponse chat(@RequestParam String userMessage) {
+    @GetMapping(path = "/streaming")
+    public Flux<String> chatStreaming(@RequestParam String userMessage) {
         return openAIChatClient.prompt()
                 .user(userMessage)
-                .call()
-                .chatResponse();
+                .stream()
+                .content();
     }
 
 }
